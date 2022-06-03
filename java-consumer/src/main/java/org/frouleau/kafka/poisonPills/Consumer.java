c4206407a2862675f15f0ca4b3b6fbaabd0f016d
Fred R <frouleau@confluent.io>
2022-06-03 19:59:22 +0200

    Adding Avro serialization errors

--- java-consumer/src/main/java/org/frouleau/kafka/poisonPills/Consumer.java
+++ java-consumer/src/main/java/org/frouleau/kafka/poisonPills/Consumer.java
@@ -4,18 +4,14 @@ import io.confluent.kafka.serializers.KafkaAvroDeserializer;
 import io.confluent.kafka.serializers.KafkaAvroDeserializerConfig;
 import models.avro.SimpleValue;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
-import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.errors.RecordDeserializationException;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.io.IOException;
 import java.lang.invoke.MethodHandles;
 import java.time.Duration;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Properties;
 
@@ -32,6 +28,7 @@ public class Consumer {
         settings.put(ConsumerConfig.GROUP_ID_CONFIG, GROUP_ID);
         settings.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka:9092");
         settings.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        settings.put(ConsumerConfig.FETCH_MIN_BYTES_CONFIG, 100);
         settings.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         settings.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, KafkaAvroDeserializer.class);
         settings.put(KafkaAvroDeserializerConfig.SPECIFIC_AVRO_READER_CONFIG, "true");
@@ -48,9 +45,23 @@ public class Consumer {
             consumer.subscribe(List.of(KAFKA_TOPIC));
             //noinspection InfiniteLoopStatement
             while (true) {
-                final var records = consumer.poll(POLL_TIMEOUT);
-                for (var record : records) {
-                    LOGGER.info("Fetch record key={} value={}", record.key(), record.value());
+                try {
+                    final var records = consumer.poll(POLL_TIMEOUT);
+                    int count = records.count();
+                    if (count!=0) {
+                        LOGGER.info("Poll return {} records", count);
+                    }
+                    for (var record : records) {
+                        LOGGER.info("Fetch record key={} value={}", record.key(), record.value());
+                    }
+                } catch (RecordDeserializationException re) {
+                    long offset = re.offset();
+                    Throwable t = re.getCause();
+                    LOGGER.error("Failed to consumer at partition={} offset={}", re.topicPartition().partition(), offset, t);
+                    LOGGER.info("Skipping offset={}", offset);
+                    consumer.seek(re.topicPartition(), offset+1);
+                } catch (Exception e) {
+                    LOGGER.error("Failed to consumer", e);
                 }
             }
         } finally {
--- java-producer/src/main/java/org/frouleau/kafka/poisonPills/Produce.java
+++ java-producer/src/main/java/org/frouleau/kafka/poisonPills/Produce.java
@@ -3,15 +3,23 @@ package org.frouleau.kafka.poisonPills;
 import io.confluent.kafka.serializers.KafkaAvroSerializer;
 import io.confluent.kafka.serializers.KafkaAvroSerializerConfig;
 import models.avro.SimpleValue;
+import org.apache.avro.io.BinaryEncoder;
+import org.apache.avro.io.DatumWriter;
+import org.apache.avro.io.EncoderFactory;
+import org.apache.avro.specific.SpecificDatumWriter;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.ByteArraySerializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.lang.invoke.MethodHandles;
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
 import java.util.Properties;
 
 public class Produce {
@@ -21,39 +29,106 @@ public class Produce {
     private static final String DRIVER_ID = "Producer";
     private static final String TOPIC = "topic";
     private static final int NB_MESSAGES = 10;
+    private int count = 0;
 
-    private static Properties settings() {
+    private Properties settings() {
         final Properties settings = new Properties();
         settings.put(ProducerConfig.CLIENT_ID_CONFIG, DRIVER_ID);
         settings.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka:9092");
         settings.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        settings.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, KafkaAvroSerializer.class);
         settings.put(KafkaAvroSerializerConfig.SCHEMA_REGISTRY_URL_CONFIG, "http://schema-registry:8081");
         return settings;
     }
+    
+    private Properties avroSettings() {
+        final Properties settings = settings();
+        settings.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, KafkaAvroSerializer.class);
+        return settings;
+    }
 
-    public static void main(String[] args) throws IOException {
-        LOGGER.info("Starting producer");
+    private Properties rawSettings() {
+        final Properties settings = settings();
+        settings.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, ByteArraySerializer.class);
+        return settings;
+    }
+    
+    public static void main(String[] args) {
+        Produce produce = new Produce();
+        produce.sendWrongAvroProducer(10);
+    }
 
-        final KafkaProducer<String, SimpleValue> producer = new KafkaProducer<>(settings());
-
-        // Adding a shutdown hook to clean up when the application exits
-        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-            LOGGER.info("Closing producer.");
-            producer.close();
-        }));
-
-        for(int i=0; i < NB_MESSAGES; i++) {
-            String key = Integer.toString(i);
-            SimpleValue value = SimpleValue.newBuilder()
-                    .setTheName("This is message " + key)
-                    .build();
-            ProducerRecord<String, SimpleValue> producerRecord = new ProducerRecord<>(TOPIC, key, value);
-            LOGGER.info("Sending message {}", i);
-            producer.send(producerRecord);
+    void sendAvroProducer(int nb) {
+        LOGGER.info("Starting Arvo Producer");
+        try (KafkaProducer<String, SimpleValue> producer = new KafkaProducer<>(avroSettings())) {
+            for (int i=0; i < nb; i++) {
+                String key = Integer.toString(count);
+                SimpleValue value = SimpleValue.newBuilder()
+                        .setTheName("This is message " + key)
+                        .build();
+                ProducerRecord<String, SimpleValue> producerRecord = new ProducerRecord<>(TOPIC, key, value);
+                LOGGER.info("Sending message {}", count);
+                producer.send(producerRecord);
+                count++;
+            }
+            LOGGER.info("Producer flush");
+            producer.flush();
+        } finally {
+            LOGGER.info("Closing producer");
         }
-        LOGGER.info("Producer flush");
-        producer.flush();
+    }
+
+    void sendRawProducer(int nb) {
+        LOGGER.info("Starting Raw Producer");
+        try (KafkaProducer<String, byte[]> producer = new KafkaProducer<>(rawSettings())) {
+            for (int i=0; i < nb; i++) {
+                String key = Integer.toString(count);
+                String value = "This is message " + key;
+                var producerRecord = new ProducerRecord<>(TOPIC, key, value.getBytes(StandardCharsets.UTF_8));
+                LOGGER.info("Sending message {}", count);
+                producer.send(producerRecord);
+                count++;
+            }
+            LOGGER.info("Producer flush");
+            producer.flush();
+        } finally {
+            LOGGER.info("Closing producer");
+        }
+    }
+
+    void sendWrongAvroProducer(int nb) {
+        LOGGER.info("Starting AvroWrong Producer");
+        try (KafkaProducer<String, byte[]> producer = new KafkaProducer<>(rawSettings())) {
+            for (int i=0; i < nb; i++) {
+                String key = Integer.toString(count);
+                SimpleValue value = SimpleValue.newBuilder()
+                        .setTheName("This is message " + key)
+                        .build();
+                var producerRecord = new ProducerRecord<>(TOPIC, key, generateAvroValue(value, i != 3));
+                LOGGER.info("Sending message {}", count);
+                producer.send(producerRecord);
+                count++;
+            }
+            LOGGER.info("Producer flush");
+            producer.flush();
+        } catch (IOException e) {
+            LOGGER.error("Failed to send", e);
+        } finally {
+            LOGGER.info("Closing producer");
+        }
+    }
+
+    byte[] generateAvroValue(SimpleValue value, boolean valid) throws IOException {
+        int id = valid ? 1 : 0;
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        out.write(0); // Magic byte
+        out.write(ByteBuffer.allocate(4).putInt(id).array()); // Schema Id
+        DatumWriter<SimpleValue> simpleValueDatumWriter = new SpecificDatumWriter<>(SimpleValue.getClassSchema());
+        BinaryEncoder encoder = EncoderFactory.get().binaryEncoder(out, null);
+        simpleValueDatumWriter.write(value, encoder);
+        encoder.flush();
+        byte[] bytes = out.toByteArray();
+        out.close();
+        return bytes;
     }
 
 }
